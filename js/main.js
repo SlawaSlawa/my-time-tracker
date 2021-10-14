68fcc4f53ffa042c45217880f77b4780d19363f6
Slawa <slawapocht4@yandex.ru>
2021-10-14 23:30:39 +0400

    add func: addTask & mouted for localStorage

--- js/main.js
+++ js/main.js
@@ -3,8 +3,29 @@
 const app = new Vue({
 	el: '.app',
 	data: {
-		taskList: {},
-		executedTaskList: {}
-	}
+		showForm: false,
+		taskList: [],
+		executedTaskList: [],
+		taskTitle: '',
+		taskDescription: '',
+	},
+	methods: {
+		addTask() {
+			console.log('add task');
+			this.taskList.push({
+				title: this.taskTitle,
+				description: this.taskDescription
+			});
+			this.taskTitle = '';
+			this.taskDescription = '';
+			this.showForm = false;
+			localStorage.setItem('taskList', JSON.stringify(this.taskList));
+		}
+	},
+	mounted() {
+   		if (localStorage.taskList) {
+    		this.taskList = JSON.parse(localStorage.taskList);
+    	}
+  }
 });
 

9676fbd7d3a46903aecd12b53e7ee3b48d607960
Slawa <slawapocht4@yandex.ru>
2021-10-16 19:07:28 +0400

    add functions: startTimer pauseTimer stopTimer

--- js/main.js
+++ js/main.js
@@ -7,19 +7,36 @@ const app = new Vue({
 		taskList: [],
 		executedTaskList: [],
 		taskTitle: '',
-		taskDescription: '',
+		taskDescription: ''
 	},
 	methods: {
 		addTask() {
 			console.log('add task');
 			this.taskList.push({
 				title: this.taskTitle,
-				description: this.taskDescription
+				description: this.taskDescription,
+				time: '0',
+				timerID: ''
 			});
 			this.taskTitle = '';
 			this.taskDescription = '';
 			this.showForm = false;
 			localStorage.setItem('taskList', JSON.stringify(this.taskList));
+		},
+		startTimer(index) {
+			this.taskList[index].timerID = setInterval(() => {
+				this.taskList[index].time++;
+			}, 1000);
+			console.log(this.taskList[index].timerID);
+		},
+		pauseTimer(index) {
+			console.log('pauseTimer ' + index);
+			clearTimeout(this.taskList[index].timerID);
+		},
+		stopTimer(index) {
+			console.log('stopTimer ' + index);
+			clearTimeout(this.taskList[index].timerID);
+			this.taskList[index].time = 0;
 		}
 	},
 	mounted() {
